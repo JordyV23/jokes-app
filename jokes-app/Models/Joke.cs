b35047b8771e04ecbc49d35bb0b71ca9f3868fa5
JordyV23 <jordyvargas0123@gmail.com>
2023-11-04 09:08:30 -0600

    Correccion de error al generar el controlador

---
+++ jokes-app/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace jokes_app.Models
+{
+    public class Joke
+    {
+        // Declaracion de propiedades de la clase Joke
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        // Declaracion de constructor
+        public Joke()
+        {
+            
+        }
+    }
+}
